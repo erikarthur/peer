cd6f26f6ad122f8cb3e17839735c9c15ba2d0441
erikarthur <erikarthur@hotmail.com>
2013-10-17 10:02:06 -0500

    first commit

---
+++ peer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+             
+            Int32 port = 13000;
+            
+            string message;
+
+            if (args.Count() < 1)
+            {
+                Console.WriteLine("Usage:  peer \"message\" ");
+                return;
+            }
+
+            message = args[0];
+
+            TcpClient client = new TcpClient("127.0.0.1", port);
+
+            // Translate the passed message into ASCII and store it as a Byte array.
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);         
+
+            // Get a client stream for reading and writing. 
+            //  Stream stream = client.GetStream();
+
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer. 
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", message);         
+
+            // Receive the TcpServer.response. 
+
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);         
+
+            // Close everything.
+            stream.Close();         
+            client.Close();         
+            
+           
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
